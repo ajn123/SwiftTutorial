b16f89c6058ce8da4310d6b530360b1e327916a0
ajn123 <ajn123@vt.edu>
2015-04-06 03:19:32 +0200

    type aliases added

---
+++ type_aliases.playground/section-1.swift
@@ -0,0 +1,13 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+
+/*
+Type aliases define an alternative name for an 
+existing type
+*/
+typealias hello = UInt8
+
+var a: hello
+a = 23
